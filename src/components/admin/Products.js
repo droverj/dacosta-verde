c653c021b65818f0446ce2209a7176b441194c81
Jodie Drover <jodiedrover@live.com>
2023-11-29 13:27:26 -0500

    Add an edit function to Products

--- src/components/admin/Products.js
+++ src/components/admin/Products.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs, deleteDoc, doc, getFirestore } from 'firebase/firestore';
-import { getStorage, ref, getDownloadURL, deleteObject } from 'firebase/storage';
+import { collection, getDocs, deleteDoc, doc, setDoc, getDoc, getFirestore } from 'firebase/firestore';
+import { getStorage, ref, getDownloadURL, deleteObject, uploadBytes } from 'firebase/storage';
 import AddProductForm from './AddProductForm';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [editedProduct, setEditedProduct] = useState({
+    label: '',
+    weight: '',
+    quantity: '',
+    pricePerPound: '',
+    image: null,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,7 +61,6 @@ const Products = () => {
     setSelectedProduct(productId);
     setShowConfirmationModal(true);
   };
-
   const handleDeleteConfirm = async () => {
     try {
       // Delete product document from Firestore
@@ -79,10 +86,67 @@ const Products = () => {
     }
   };
 
-  const handleDeleteCancel = () => {
-    // Reset selected product and hide the confirmation modal
+  const handleEditClick = async (product) => {
+    setSelectedProduct(product.id);
+    setShowEditModal(true);
+
+    // Fetch the current product data from Firestore
+    const db = getFirestore();
+    const productDocRef = doc(db, 'products', product.id);
+    const productDoc = await getDoc(productDocRef);
+
+    if (productDoc.exists()) {
+      const productData = productDoc.data();
+      setEditedProduct({
+        label: productData.label || '',
+        weight: productData.weight || '',
+        quantity: productData.quantity || '',
+        pricePerPound: productData.pricePerPound || '',
+        image: null, // Note: To be updated if needed
+      });
+    }
+  };
+
+  const handleEditConfirm = async () => {
+    try {
+      // Update product document in Firestore
+      const db = getFirestore();
+      const productDocRef = doc(db, 'products', selectedProduct);
+      await setDoc(productDocRef, {
+        label: editedProduct.label,
+        weight: editedProduct.weight,
+        quantity: editedProduct.quantity,
+        pricePerPound: editedProduct.pricePerPound,
+      });
+
+      // Reset edited product and hide the edit modal
+      setEditedProduct({
+        label: '',
+        weight: '',
+        quantity: '',
+        pricePerPound: '',
+        image: null,
+      });
+      setSelectedProduct(null);
+      setShowEditModal(false);
+
+      console.log('Product edited successfully!');
+    } catch (error) {
+      console.error('Error editing product:', error.message);
+    }
+  };
+
+  const handleEditCancel = () => {
+    // Reset edited product and hide the edit modal
+    setEditedProduct({
+      label: '',
+      weight: '',
+      quantity: '',
+      pricePerPound: '',
+      image: null,
+    });
     setSelectedProduct(null);
-    setShowConfirmationModal(false);
+    setShowEditModal(false);
   };
 
   return (
@@ -99,6 +163,7 @@ const Products = () => {
             <p>Price per Pound: {product.pricePerPound}</p>
             {product.imageUrl && <img src={product.imageUrl} alt={product.label} style={{ maxWidth: '100px' }} />}
             <button onClick={() => handleDeleteClick(product.id)}>Delete</button>
+            <button onClick={() => handleEditClick(product)}>Edit</button>
           </li>
         ))}
       </ul>
@@ -107,7 +172,51 @@ const Products = () => {
         <div>
           <p>Are you sure you want to delete this product?</p>
           <button onClick={handleDeleteConfirm}>Yes</button>
-          <button onClick={handleDeleteCancel}>No</button>
+          <button onClick={handleEditCancel}>No</button>
+        </div>
+      )}
+
+      {showEditModal && (
+        <div>
+          <h2>Edit Product</h2>
+          <label>
+            Label:
+            <input
+              type="text"
+              value={editedProduct.label}
+              onChange={(e) => setEditedProduct((prev) => ({ ...prev, label: e.target.value }))}
+            />
+          </label>
+
+          <label>
+            Weight:
+            <input
+              type="text"
+              value={editedProduct.weight}
+              onChange={(e) => setEditedProduct((prev) => ({ ...prev, weight: e.target.value }))}
+            />
+          </label>
+
+          <label>
+            Quantity:
+            <input
+              type="text"
+              value={editedProduct.quantity}
+              onChange={(e) => setEditedProduct((prev) => ({ ...prev, quantity: e.target.value }))}
+            />
+          </label>
+
+          <label>
+            Price per Pound:
+            <input
+              type="text"
+              value={editedProduct.pricePerPound}
+              onChange={(e) => setEditedProduct((prev) => ({ ...prev, pricePerPound: e.target.value }))}
+            />
+          </label>
+
+          <button onClick={handleEditConfirm}>Save Changes</button>
+          <button onClick={handleEditCancel}>Cancel</button>
         </div>
       )}
     </div>
