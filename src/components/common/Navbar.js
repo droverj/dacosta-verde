b1e9376bcbc271025c4226a305c6441c8d7be8af
Jodie Drover <jodiedrover@live.com>
2023-12-05 23:27:03 -0500

    Add navbar links

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <Link to='/'><h1>DaCosta Verde</h1></Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='contact'>Contact</Link>
+          </li>
+          <li>
+            <Link to='/shop'>Shop</Link>
+          </li>
+          <li>
+            <Link to='/cart'>Cart</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
