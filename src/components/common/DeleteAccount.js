4e9c0882ebbc7f9a0ad85a0e5b58465bdf9b200a
Jodie Drover <jodiedrover@live.com>
2023-12-09 21:12:16 -0500

    Delete user from users db in firestore

--- src/components/common/DeleteAccount.js
+++ src/components/common/DeleteAccount.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import { useAuth } from '../../hooks/AuthProvider';
 import { deleteUser } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { doc, deleteDoc } from 'firebase/firestore';
+import { db } from '../../firebase-configs/firebase-config';
 
 const DeleteAccount = ({ setDeleteUser }) => {
   const { user } = useAuth();
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleDeleteAccount = async () => {
     try {
+      // Delete the user from Firestore
+      const userDocRef = doc(db, 'users', user.uid);
+      await deleteDoc(userDocRef);
+
       // Delete the user from Firebase Authentication
       await deleteUser(user);
 
       // User deleted successfully
       setError(null);
       console.log('User deleted successfully!');
+      navigate('/');
     } catch (error) {
       // Handle deletion error
       setError(error.message);
