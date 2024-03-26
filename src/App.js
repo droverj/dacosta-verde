1c351dd9dcc2fcc69593c06735318223be9f2385
Jodie Drover <jodiedrover@live.com>
2024-03-26 12:59:30 -0400

    feat: create Header and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 import './App.scss';
 
 const App = () => {
   return (
-    <div className='App'>DaCosta Verde</div>
+    <div className='App'>
+      <Header />
+      <Navbar />
+    </div>
   )
 }
 
