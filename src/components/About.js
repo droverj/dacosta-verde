2a06b773fd93510aa9d996d6587e62ff14361119
Jodie Drover <jodiedrover@live.com>
2024-03-28 12:00:40 -0400

    feat: stage other pages

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/About.scsss'
+
+const About = () => {
+  return (
+    <div className='About'>
+      About
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Products.scss'
+
+const Products = () => {
+  return (
+    <div className='Products'>
+      Products
+    </div>
+  )
+}
+
+export default Products
